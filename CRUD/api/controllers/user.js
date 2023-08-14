2e4cbcf3e2f71c321cee9aaecd31bb2e095af3e5
pedroLimaIFPA <pedrolimaifpa@gmail.com>
2023-08-14 14:13:06 -0300

    commit 5

--- CRUD/api/controllers/user.js
+++ CRUD/api/controllers/user.js
@@ -11,8 +11,7 @@ export const getUsers = (_, res) => {
 )};
 
 export const addUser = (req, res) => {
-    const q =
-    "INSERT INTO usuarios(`nome`, `email`,`fone`, `idade` VALUES(?)";
+    const q = "INSERT INTO usuarios(`nome`, `email`,`fone`, `idade` VALUES(?)";
 
     const values = [
         req.body.nome,
@@ -30,8 +29,7 @@ export const addUser = (req, res) => {
 
 
 export const updateUser = (req, res) => {
-    const q =
-    "UPDATE usuarios SET `nome` = ?, `email` = ?,`fone` = ?, `idade` = ? WHERE `id` = ?";
+    const q = "UPDATE usuarios SET `nome` = ?, `email` = ?,`fone` = ?, `idade` = ? WHERE `id` = ?";
 
     const values = [
         req.body.nome,
@@ -48,8 +46,7 @@ export const updateUser = (req, res) => {
 }
 
 export const deleteUser = (req, res) => {
-    const q =
-    "DELETE usuarios WHERE `id` = ?";
+    const q = "DELETE usuarios WHERE `id` = ?";
 
     db.query(q, [req.params.id], (err) =>{
       if (err) return res.json(err);
